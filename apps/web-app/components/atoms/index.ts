c39f93db273352aa94420daa57c6b0680cddccdd
Andrzej Pacholik <andrzej.pacholik@gmail.com>
2023-09-30 19:24:35 +0200

    feat(app): add dialog component

--- apps/web-app/components/atoms/index.ts
+++ apps/web-app/components/atoms/index.ts
@@ -1,4 +1,5 @@
 // DO NOT EDIT: File generated automatically!
 export { default as Button } from "./Button";
 export { default as Card } from "./Card";
+export { default as Dialog } from "./Dialog";
 export { default as Tabs } from "./Tabs";
