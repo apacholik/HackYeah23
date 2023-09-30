39afae1673fa5ea35c4fb50d9ae34a694dc66db7
Andrzej Pacholik <andrzej.pacholik@gmail.com>
2023-10-01 00:30:59 +0200

    feat(app): add fancy thanks

--- apps/web-app/pages/animal-report/thanks.tsx
+++ apps/web-app/pages/animal-report/thanks.tsx
@@ -1,8 +1,11 @@
+import Image from "next/image";
+
 export default function AnimalReportThanks() {
   return (
-    <div className="flex justify-center items-center h-full">
-      <div className="border border-green-800 bg-green-200 text-green-900 p-6 rounded-md font-bold text-center w-2/3">
-        Dziękujemy za zgłoszenie!
+    <div className="flex justify-center items-center h-full flex-col">
+      <Image className="w-1/3" src="/assets/img/ok.png" alt="alright!" width="300" height="300" />
+      <div className="border border-green-800 bg-green-200 text-green-900 p-6 rounded-md font-bold text-center w-1/3">
+        Zgłoszenie przyjęte!
       </div>
     </div>
   );
